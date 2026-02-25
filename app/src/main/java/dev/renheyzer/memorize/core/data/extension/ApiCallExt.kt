0c060902ac916da6d9ae6caecc610c14bab7540f
RenHeyzer <vesolyyradin@gmail.com>
2026-02-25 21:51:09 +0600

    fix(core/data): add check for CancellationException and rethrow it

--- app/src/main/java/dev/renheyzer/memorize/core/data/extension/ApiCallExt.kt
+++ app/src/main/java/dev/renheyzer/memorize/core/data/extension/ApiCallExt.kt
@@ -2,13 +2,14 @@ package dev.renheyzer.memorize.core.data.extension
 
 import dev.renheyzer.memorize.core.common.NetworkError
 import dev.renheyzer.memorize.core.common.Either
-import java.io.IOException
+import kotlinx.coroutines.CancellationException
 
 suspend fun <T> safeApiCall(apiCall: suspend () -> T): Either<NetworkError, T> {
     return try {
         val result = apiCall()
         Either.Right(result)
     } catch (e: Exception) {
+        if (e is CancellationException) throw e
         val error = e.toNetworkType()
         Either.Left(error)
     }
