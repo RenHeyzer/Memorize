a86646c3ac4acb12ba832cacf7abf699202a3aa5
RenHeyzer <vesolyyradin@gmail.com>
2026-02-28 02:56:19 +0600

    refactor(core/home/presentation): replace DefaultCoreRootComponent with HomeComponent

--- app/src/main/java/dev/renheyzer/memorize/feature/core/home/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/home/presentation/ui/home/HomeScreen.kt
@@ -5,9 +5,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import dev.renheyzer.memorize.core.components.core.CoreRootComponent
+import dev.renheyzer.memorize.core.components.core.home.HomeComponent
 
 @Composable
-fun HomeScreen(component: CoreRootComponent, modifier: Modifier = Modifier) {
+fun HomeScreen(component: HomeComponent, modifier: Modifier = Modifier) {
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
         
     }
