585765a6472882fe8f73bf5457243d713105ee3e
RenHeyzer <vesolyyradin@gmail.com>
2026-02-27 23:57:31 +0600

    feat(numbers): add CheckAnswersUseCase

---
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/numbers/domain/usecase/CheckAnswersUseCase.kt
@@ -0,0 +1,32 @@
+package dev.renheyzer.memorize.feature.core.numbers.domain.usecase
+
+import dev.renheyzer.memorize.feature.core.numbers.domain.model.AnswerResult
+import dev.renheyzer.memorize.feature.core.numbers.domain.model.GameResult
+
+class CheckAnswersUseCase {
+
+    operator fun invoke(numbers: List<Int>, answers: List<Int>): GameResult {
+        val details = numbers.mapIndexed { i, number ->
+            val answer = answers.getOrNull(i)
+            AnswerResult(
+                id = i,
+                number = number,
+                answer = answer,
+                isCorrect = number == answer
+            )
+        }
+
+        val correctCount = details.count { it.isCorrect }
+        val totalCount = numbers.size
+        val scorePercentage = if (totalCount > 0) {
+            (correctCount * 100f) / totalCount
+        } else 0f
+
+        return GameResult(
+            details = details,
+            correctCount = correctCount,
+            totalCount = totalCount,
+            scorePercentage = scorePercentage
+        )
+    }
+}
