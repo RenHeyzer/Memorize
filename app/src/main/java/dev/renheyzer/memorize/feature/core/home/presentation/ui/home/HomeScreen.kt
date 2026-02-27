a1a8b90e239bc4ac501c6c17ed28529fdddae247
RenHeyzer <vesolyyradin@gmail.com>
2026-02-27 23:41:34 +0600

    refactor(feature/core): move HomeScreen to home submodule
    HomeScreen has been moved to feature/core/home to avoid cluttering feature/core and improve code organization

--- app/src/main/java/dev/renheyzer/memorize/feature/core/home/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/home/presentation/ui/home/HomeScreen.kt
@@ -1,13 +1,13 @@
-package dev.renheyzer.memorize.feature.core.presentation.ui.home
+package dev.renheyzer.memorize.feature.core.home.presentation.ui.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import dev.renheyzer.memorize.core.components.core.HomeComponent
+import dev.renheyzer.memorize.core.components.core.CoreRootComponent
 
 @Composable
-fun HomeScreen(component: HomeComponent, modifier: Modifier = Modifier) {
+fun HomeScreen(component: CoreRootComponent, modifier: Modifier = Modifier) {
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
         
     }
