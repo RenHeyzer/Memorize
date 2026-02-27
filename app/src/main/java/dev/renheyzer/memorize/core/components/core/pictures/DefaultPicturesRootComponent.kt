9e7588b4ae55036353afce7143f69684f17005af
RenHeyzer <vesolyyradin@gmail.com>
2026-02-28 01:25:16 +0600

    feat(components/core): add DefaultPicturesRootComponent

---
+++ app/src/main/java/dev/renheyzer/memorize/core/components/core/pictures/DefaultPicturesRootComponent.kt
@@ -0,0 +1,11 @@
+package dev.renheyzer.memorize.core.components.core.pictures
+
+import com.arkivanov.decompose.ComponentContext
+import dev.renheyzer.memorize.core.ui.decompose.ComponentEnvironment
+
+class DefaultPicturesRootComponent(
+    componentContext: ComponentContext,
+    private val env: ComponentEnvironment
+): PicturesRootComponent, ComponentContext by componentContext {
+
+}
