15817806b0a02d668f8dd3a6e94c7ee4ddf9f5ab
RenHeyzer <vesolyyradin@gmail.com>
2026-02-27 22:24:42 +0600

    feat(core/di): recreate class and add by lazy to all dependencies
    All dependencies were wrapped in lazy(LazyThreadSafetyMode.None) for maximum performance, so as not to synchronize when accessed from different threads.

---
+++ app/src/main/java/dev/renheyzer/memorize/core/di/auth/AuthDependenciesImpl.kt
@@ -0,0 +1,50 @@
+package dev.renheyzer.memorize.core.di.auth
+
+import com.google.firebase.auth.FirebaseAuth
+import dev.renheyzer.memorize.BuildConfig
+import dev.renheyzer.memorize.core.models.AuthConfig
+import dev.renheyzer.memorize.feature.auth.data.remote.source.AuthRemoteDataSource
+import dev.renheyzer.memorize.feature.auth.data.remote.source.AuthRemoteDataSourceImpl
+import dev.renheyzer.memorize.feature.auth.data.repositories.AuthRepository
+import dev.renheyzer.memorize.feature.auth.data.repositories.AuthRepositoryImpl
+import dev.renheyzer.memorize.feature.auth.domain.usecase.registration.CompositeValidator
+import dev.renheyzer.memorize.feature.auth.domain.usecase.registration.RegisterByEmailUseCase
+import dev.renheyzer.memorize.feature.auth.domain.usecase.registration.ValidateEmailUseCase
+import dev.renheyzer.memorize.feature.auth.domain.usecase.registration.ValidatePasswordUseCase
+
+class AuthDependenciesImpl(private val firebaseAuth: FirebaseAuth) : AuthDependencies {
+
+    override val authConfig: AuthConfig by lazy(LazyThreadSafetyMode.NONE) {
+        AuthConfig(deepLinkUrl = BuildConfig.VERIFICATION_DEEP_LINK_URL)
+    }
+
+    override val authRemoteDataSource: AuthRemoteDataSource by lazy(LazyThreadSafetyMode.NONE) {
+        AuthRemoteDataSourceImpl(firebaseAuth)
+    }
+
+    override val authRepository: AuthRepository by lazy(LazyThreadSafetyMode.NONE) {
+        AuthRepositoryImpl(authRemoteDataSource, authConfig)
+    }
+
+    override val validateEmailUseCase by lazy(LazyThreadSafetyMode.NONE) {
+        ValidateEmailUseCase()
+    }
+
+    override val validatePasswordUseCase by lazy(LazyThreadSafetyMode.NONE) {
+        ValidatePasswordUseCase()
+    }
+
+    override val compositeValidator by lazy(LazyThreadSafetyMode.NONE) {
+        CompositeValidator(
+            validateEmail = validateEmailUseCase,
+            validatePassword = validatePasswordUseCase,
+        )
+    }
+
+    override val registerByEmailUseCase by lazy(LazyThreadSafetyMode.NONE) {
+        RegisterByEmailUseCase(
+            compositeValidator = compositeValidator,
+            repository = authRepository
+        )
+    }
+}
