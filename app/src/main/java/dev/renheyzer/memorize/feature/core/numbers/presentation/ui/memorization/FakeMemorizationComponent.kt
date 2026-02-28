e4abb6e846895e05df30db1d0f2ed43aeaed5f3c
RenHeyzer <vesolyyradin@gmail.com>
2026-02-28 23:14:06 +0600

    feat(numbers): implement fake MemorizationComponent to test UI

---
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/numbers/presentation/ui/memorization/FakeMemorizationComponent.kt
@@ -0,0 +1,20 @@
+package dev.renheyzer.memorize.feature.core.numbers.presentation.ui.memorization
+
+import dev.renheyzer.memorize.core.components.core.numbers.memorization.MemorizationComponent
+import dev.renheyzer.memorize.core.components.core.numbers.memorization.store.MemorizationUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlin.random.Random
+
+class FakeMemorizationComponent : MemorizationComponent {
+    override val uiState: StateFlow<MemorizationUiState> = MutableStateFlow(
+        MemorizationUiState(
+            numbers = List(27) { Random.nextInt(28) }.chunked(9),
+            quantity = 27,
+            isRandom = true,
+            timerValue = "01:56"
+        )
+    )
+
+    override fun onCompleteClick() = Unit
+}
