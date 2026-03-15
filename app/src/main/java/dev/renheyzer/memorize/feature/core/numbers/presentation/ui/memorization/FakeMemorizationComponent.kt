884d911a2413a5472c46414b6f57a5ff1db95afd
RenHeyzer <vesolyyradin@gmail.com>
2026-03-15 18:43:49 +0600

    refactor(numbers): remove chunking from list creation logic

--- app/src/main/java/dev/renheyzer/memorize/feature/core/numbers/presentation/ui/memorization/FakeMemorizationComponent.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/numbers/presentation/ui/memorization/FakeMemorizationComponent.kt
@@ -9,7 +9,7 @@ import kotlin.random.Random
 class FakeMemorizationComponent : MemorizationComponent {
     override val uiState: StateFlow<MemorizationUiState> = MutableStateFlow(
         MemorizationUiState(
-            numbers = List(27) { Random.nextInt(28) }.chunked(9),
+            numbers = List(27) { Random.nextInt(28) },
             quantity = 27,
             isRandom = true,
         )
