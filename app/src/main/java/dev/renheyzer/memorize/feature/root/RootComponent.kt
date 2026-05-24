4deb0fd68faf952f067188d46aabdd94a2400bba
RenHeyzer <vesolyyradin@gmail.com>
2026-05-24 18:04:57 +0600

    refactor(root): move RootComponent to feature package
    - Relocate RootComponent from core.components to the feature.root package.
    - Update imports for AuthComponent and CoreRootComponent to reflect their new locations within the feature package structure.

--- app/src/main/java/dev/renheyzer/memorize/feature/root/RootComponent.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/root/RootComponent.kt
@@ -1,10 +1,10 @@
-package dev.renheyzer.memorize.core.components.root
+package dev.renheyzer.memorize.feature.root
 
 import android.net.Uri
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
-import dev.renheyzer.memorize.core.components.auth.AuthComponent
-import dev.renheyzer.memorize.core.components.core.CoreRootComponent
+import dev.renheyzer.memorize.feature.auth.presentation.component.AuthComponent
+import dev.renheyzer.memorize.feature.core.presenatation.component.CoreRootComponent
 
 interface RootComponent {
 
