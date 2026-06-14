49aa4fe8aa8b2d19f3b54bf7dd3d7b23ede105e5
RenHeyzer <vesolyyradin@gmail.com>
2026-06-14 18:16:51 +0600

    refactor(cards): rename PlayingCard to Card and update domain model imports
    - Rename `PlayingCard` model to `Card` within `GenerateOrderedDeckUseCase`.
    - Update imports for `Card`, `CardRank`, and `CardSuit` to reflect their relocation to the `dev.renheyzer.memorize.feature.core.cards.domain.model` package.

--- app/src/main/java/dev/renheyzer/memorize/feature/core/cards/domain/usecase/GenerateOrderedDeckUseCase.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/cards/domain/usecase/GenerateOrderedDeckUseCase.kt
@@ -1,18 +1,18 @@
 package dev.renheyzer.memorize.feature.core.cards.domain.usecase
 
-import CardRank
-import CardSuit
-import PlayingCard
+import dev.renheyzer.memorize.feature.core.cards.domain.model.Card
+import dev.renheyzer.memorize.feature.core.cards.domain.model.CardRank
+import dev.renheyzer.memorize.feature.core.cards.domain.model.CardSuit
 
 class GenerateOrderedDeckUseCase {
-    operator fun invoke(suits: List<CardSuit>): List<PlayingCard> {
+    operator fun invoke(suits: List<CardSuit>): List<Card> {
         if (suits.isEmpty()) return emptyList()
 
         val sortedSuits = suits.sortedBy { it.sortOrder }
 
         return sortedSuits.flatMap { suit ->
             CardRank.entries.map { rank ->
-                PlayingCard(
+                Card(
                     suit = suit,
                     rank = rank
                 )
