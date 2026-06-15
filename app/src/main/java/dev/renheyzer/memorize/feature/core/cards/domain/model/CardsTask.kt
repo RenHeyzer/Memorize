7591037c7df113b084cee60df7a72f47322ddf5c
RenHeyzer <vesolyyradin@gmail.com>
2026-06-15 23:03:01 +0600

    refactor(cards): update `CardsTask` model and `ShuffleDeckUseCase` return type
    - Rename `cards` property to `deck` in `CardsTask` data class.
    - Update `ShuffleDeckUseCase` to return a `CardsTask` object instead of a `List<Card>`.
    - Replace the empty list check in `ShuffleDeckUseCase` with a `require` block to enforce non-empty decks.

--- app/src/main/java/dev/renheyzer/memorize/feature/core/cards/domain/model/CardsTask.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/cards/domain/model/CardsTask.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CardsTask(
-    val cards: List<Card>
+    val deck: List<Card>
 )
--- app/src/main/java/dev/renheyzer/memorize/feature/core/cards/domain/usecase/ShuffleDeckUseCase.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/cards/domain/usecase/ShuffleDeckUseCase.kt
@@ -1,16 +1,20 @@
 package dev.renheyzer.memorize.feature.core.cards.domain.usecase
 
 import dev.renheyzer.memorize.feature.core.cards.domain.model.Card
+import dev.renheyzer.memorize.feature.core.cards.domain.model.CardsTask
 import java.util.Random
 
 class ShuffleDeckUseCase {
-    operator fun invoke(deck: List<Card>, random: Random? = null): List<Card> {
-        if (deck.isEmpty()) return emptyList()
+    operator fun invoke(deck: List<Card>, random: Random? = null): CardsTask {
+        require(deck.isNotEmpty()) {
+            "Deck cannot be empty"
+        }
 
-        return if (random != null) {
+        val deck = if (random != null) {
             deck.shuffled(random)
         } else {
             deck.shuffled()
         }
+        return CardsTask(deck)
     }
 }
