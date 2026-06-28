52f33d5f5bf881e44c6092a98ba4c9e6c2813497
RenHeyzer <vesolyyradin@gmail.com>
2026-06-28 17:24:52 +0600

    fix(cards): correct last page detection in MemorizationScreen
    - Update `isLastPage` calculation to use `pagerState.currentPage` instead of `targetPage` to ensure the correct state is reflected when the user reaches the end of the pager.

--- app/src/main/java/dev/renheyzer/memorize/feature/core/cards/presentation/ui/memorization/MemorizationScreen.kt
+++ app/src/main/java/dev/renheyzer/memorize/feature/core/cards/presentation/ui/memorization/MemorizationScreen.kt
@@ -41,7 +41,7 @@ fun MemorizationScreen(
     val scope = rememberCoroutineScope()
 
     val isPrevEnabled by remember { derivedStateOf { pagerState.currentPage != 0 } }
-    val isLastPage by remember { derivedStateOf { pagerState.targetPage == pagerState.pageCount - 1 } }
+    val isLastPage by remember { derivedStateOf { pagerState.currentPage == pagerState.pageCount - 1 } }
 
     val columnCount = remember(state.itemPerPage) { calculateColumnsCount(state.itemPerPage) }
 
